6d3577622f3db543dd4b8a75cea9e46e88c4b223
Archer <2111410956@qq.com>
2023-04-23 12:40:40 +0800

    移动

--- The Legend of Warrior/Assets/Scripts/Player/PlayerController.cs
+++ The Legend of Warrior/Assets/Scripts/Player/PlayerController.cs
@@ -7,10 +7,15 @@ public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl inputControl;
     public Vector2 inputDirection;
+    public float speed;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         inputControl = new PlayerInputControl();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable(){
@@ -23,5 +28,18 @@ public class PlayerController : MonoBehaviour
 
     private void Update() {
         inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate() {
+        move();
+    }
+
+    public void move(){
+        rb.velocity = new Vector2(inputDirection.x*speed*Time.deltaTime, rb.velocity.y);
+        
+        if (inputDirection.x > 0)
+            spriteRenderer.flipX = false;
+        if (inputDirection.x < 0)
+            spriteRenderer.flipX = true;
     }
 }
