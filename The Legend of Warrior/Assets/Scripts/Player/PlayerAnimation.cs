584ec493b6ef32e3f6c982a26aa7afa81a061eec
Archer <2111410956@qq.com>
2023-04-23 16:42:26 +0800

    跳跃动画

--- The Legend of Warrior/Assets/Scripts/Player/PlayerAnimation.cs
+++ The Legend of Warrior/Assets/Scripts/Player/PlayerAnimation.cs
@@ -6,10 +6,12 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+    private PhysicsCheck physicsCheck;
 
     private void Awake() {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        physicsCheck = GetComponent<PhysicsCheck>();
     }
 
     private void Update() {
@@ -18,5 +20,7 @@ public class PlayerAnimation : MonoBehaviour
 
     public void SetAnimation(){
         anim.SetFloat("velocityX", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("velocityY", rb.velocity.y);
+        anim.SetBool("isGround", physicsCheck.isGround);
     }
 }
