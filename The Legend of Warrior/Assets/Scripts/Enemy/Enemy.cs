79bbe38939bd327367d5686c9ca311f1d1243c90
Archer <2111410956@qq.com>
2023-04-25 12:05:57 +0800

    敌人受伤和死亡

--- The Legend of Warrior/Assets/Scripts/Enemy/Enemy.cs
+++ The Legend of Warrior/Assets/Scripts/Enemy/Enemy.cs
@@ -6,26 +6,90 @@ public class Enemy : MonoBehaviour
 {
     Rigidbody2D rb;
     protected Animator anim;
+    PhysicsCheck physicsCheck;
+    [Header("基本参数")]
     public float normalSpeed;
     public float chaseSpeed;
     public float currentSpeed;
     public Vector3 faceDir;
+    public float hurtForce;
+    public Transform attacker;
+
+    [Header("计时器")]
+    public float waitTime;
+    public float waitTimeCounter;
+    public bool wait;
+
+    [Header("状态")]
+    public bool isHurt;
+    public bool isDead;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        physicsCheck = GetComponent<PhysicsCheck>();
         currentSpeed = normalSpeed;
+        waitTimeCounter = waitTime;
     }
 
     private void Update() {
         faceDir = new Vector3(-transform.localScale.x,0,0);
+
+        if ((physicsCheck.touchLeftWall && faceDir.x < 0)|| (physicsCheck.touchRightWall && faceDir.x > 0)){
+            wait = true;
+            anim.SetBool("walk",false);
+        }
+
+        TimeCounter();
     }
 
     private void FixedUpdate() {
-        Move();
+        if (!isHurt && !isDead)
+            Move();
     }
 
     public virtual void Move(){
         rb.velocity = new Vector2(currentSpeed*faceDir.x*Time.deltaTime,rb.velocity.y);
+    }
+
+    public void TimeCounter(){
+        if (wait){
+            waitTimeCounter -= Time.deltaTime;
+            if (waitTimeCounter <= 0){
+                wait = false;
+                waitTimeCounter = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        } 
+    }
+
+    public void OnTakeDamage(Transform attackerTrans){
+        attacker = attackerTrans;
+        if(attackerTrans.position.x - transform.position.x>0)
+            transform.localScale = new Vector3(-1,1,1);
+        if (attackerTrans.position.x - transform.position.x<0)
+            transform.localScale = new Vector3(1,1,1);
+
+        isHurt = true;
+        anim.SetTrigger("hurt");
+        Vector2 dir = new Vector2(transform.position.x - attackerTrans.position.x, 0).normalized;
+
+        StartCoroutine(OnHurt(dir));
+    }
+
+    private IEnumerator OnHurt(Vector2 dir){
+        rb.AddForce(dir*hurtForce,ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.5f);
+        isHurt = false;
+    }
+
+    public void Ondie(){
+        gameObject.layer = 2;
+        anim.SetBool("dead", true);
+        isDead = true;
+    }
+
+    public void DestroyAfterAnimation(){
+        Destroy(this.gameObject);
     }
 }
--- The Legend of Warrior/Assets/Scripts/General/PhysicsCheck.cs
+++ The Legend of Warrior/Assets/Scripts/General/PhysicsCheck.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
+    private CapsuleCollider2D coll;
+    public bool manual;
+
     [Header("检测参数")] 
     public Vector2 bottomOffset;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
     public float checkRadius;
     public LayerMask groundLayer;
     [Header("状态")] 
     public bool isGround;
+    public bool touchLeftWall;
+    public bool touchRightWall;
+
+    private void Awake() {
+        coll = GetComponent<CapsuleCollider2D>();
+        if (!manual){
+            rightOffset = new Vector2((coll.bounds.size.x+coll.offset.x)/2,coll.bounds.size.y/2);
+            leftOffset = new Vector2(-rightOffset.x, rightOffset.y);
+        }
+    }
+
     private void Update() {
         Check();
     }
@@ -17,9 +33,14 @@ public class PhysicsCheck : MonoBehaviour
     public void Check() {
         //检查地面
         isGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset*transform.localScale, checkRadius, groundLayer);
+
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRadius, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRadius, groundLayer);
     }
 
     private void OnDrawGizmosSelected() {
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset*transform.localScale, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset*transform.localScale, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset*transform.localScale, checkRadius);
     }
 }
